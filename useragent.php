429b33d2f0c7fe1d769863db9817e07fd90341aa
Rene Kreijveld <email@renekreijveld.nl>
2015-04-14 16:49:20 +0200

    Better code formatting

--- useragent.php
+++ useragent.php
@@ -5,18 +5,18 @@
  *              Based on the excellent work of Pieter-Jan de Vries, Open Source Design
  */
 
-defined('_JEXEC') or die();
+defined( '_JEXEC' ) or die();
 
-jimport('joomla.plugin.plugin');
+jimport( 'joomla.plugin.plugin' );
 
 class plgContentUseragent extends JPlugin
 {
 	/*
 	 * Test device and render content if proper device is detected.
 	 */
-	private function processDevice($text, $ualayout, $layout) {
+	private function processDevice( $text, $ualayout, $layout ) {
 		// Search for this tag in the content
-		switch ($layout) {
+		switch ( $layout ) {
 			case 'desktop':
 				$regex = "#{(!?)ifdesktop}(.*?){/ifdesktop}#s";
 				break;			
@@ -27,32 +27,32 @@ class plgContentUseragent extends JPlugin
 				$regex = "#{(!?)ifmobile}(.*?){/ifmobile}#s";
 				break;			
 		}
-		$isDevice = ($ualayout == $layout ? true : false);
+		$isDevice = ( $ualayout == $layout ? true : false );
 
-		$text = preg_replace_callback($regex, function ($match) use ($isDevice) {
-			if ($match[1]) {
+		$text = preg_replace_callback( $regex, function ( $match ) use ( $isDevice ) {
+			if ( $match[1] ) {
 				return $isDevice ? '' : $match[2];
 			} else {
 				return $isDevice ? $match[2] : '';
 			}
-		}, $text);
+		}, $text );
 
 		return $text;
 	}
 
-	public function onContentPrepare($context, &$article, &$params, $page=0) {
-		if (!class_exists('Mobile_Detect')) {
-			include_once(dirname(__FILE__).'/lib/Mobile_Detect.php');
+	public function onContentPrepare( $context, &$article, &$params, $page=0 ) {
+		if ( !class_exists( 'Mobile_Detect' ) ) {
+			include_once( dirname(__FILE__) . '/lib/Mobile_Detect.php' );
 		}
 		$detect = new Mobile_Detect();
-		$ualayout = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'mobile') : 'desktop');
-		if (($detect->is('Bot')) || ($detect->is('MobileBot'))) $ualayout = 'bot';
+		$ualayout = ( $detect->isMobile() ? ( $detect->isTablet() ? 'tablet' : 'mobile' ) : 'desktop' );
+		if ( ( $detect->is( 'Bot' ) ) || ( $detect->is( 'MobileBot' ) ) ) $ualayout = 'bot';
 
 		$this->article = $article;
 
-		if (isset($article->text)) {
+		if ( isset( $article->text ) ) {
 			$text = $article->text;
-		} else if (isset($article->introtext)) {
+		} else if ( isset($article->introtext) ) {
 			$text = $article->introtext;
 		} else {
 			$text = '';
@@ -60,22 +60,22 @@ class plgContentUseragent extends JPlugin
 
 		if (!empty($text)) {
 			// Check whether the plugin should process or not
-			if (JString::strpos($text, '{ifdesktop') !== false || JString::strpos($text, '{!ifdesktop')) {
-				$text = $this->processDevice($text, $ualayout, "desktop");
+			if ( JString::strpos( $text, '{ifdesktop' ) !== false || JString::strpos( $text, '{!ifdesktop' ) ) {
+				$text = $this->processDevice( $text, $ualayout, "desktop" );
 			}
 
-			if (JString::strpos($text, '{iftablet') !== false || JString::strpos($text, '{!iftablet')) {
-				$text = $this->processDevice($text, $ualayout, "tablet");
+			if ( JString::strpos( $text, '{iftablet') !== false || JString::strpos( $text, '{!iftablet' ) ) {
+				$text = $this->processDevice( $text, $ualayout, "tablet" );
 			}
 
-			if (JString::strpos($text, '{ifmobile') !== false || JString::strpos($text, '{!ifmobile')) {
-				$text = $this->processDevice($text, $ualayout, "mobile");
+			if ( JString::strpos( $text, '{ifmobile') !== false || JString::strpos( $text, '{!ifmobile' ) ) {
+				$text = $this->processDevice( $text, $ualayout, "mobile" );
 			}
 
-			if (isset($article->introtext)){
+			if ( isset( $article->introtext ) ){
 				$article->introtext = $text;
 			}
-			if (isset($article->text)){
+			if ( isset( $article->text ) ){
 				$article->text = $text;
 			}
 		}
